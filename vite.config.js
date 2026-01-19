2e110a832f2e414fbf7628a57ef2d3d09ec1f54e
elijaahInverted <darksoothingshadow@gmail.com>
2026-01-19 13:45:39 +0000

    chore: cleanup github pages config for migration to Vercel/Netlify

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,13 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base: '/reis-admin/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
         },
     },
     server: {
-        port: 8080, // Keep original port
+        port: 8080,
     }
 });
